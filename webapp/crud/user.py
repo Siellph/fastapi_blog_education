20ac0fb46dada46ef25a35588fd3358ccbbc367b
Vladislav Gordin <vl241095@gmail.com>
2024-02-01 01:09:42 +0500

    crud операции, добавлен minio в poetry

--- webapp/crud/user.py
+++ webapp/crud/user.py
@@ -2,8 +2,37 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from webapp.models.sirius.user import User as SQLAUser
-from webapp.schema.login.user import User as PydanticUser, UserLogin
+from webapp.schema.login.user import User as PydanticUser, UserCreate, UserLogin
 from webapp.utils.auth.password import hash_password
+
+
+async def create_user(session: AsyncSession, user_data: UserCreate) -> PydanticUser:
+    hashed_password = hash_password(user_data.password)
+    new_user = SQLAUser(
+        username=user_data.username,
+        hashed_password=hashed_password,
+        email=user_data.email,
+        role=user_data.role,
+        additional_info=user_data.additional_info,
+    )
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return PydanticUser.model_validate(new_user)
+
+
+async def update_user(session: AsyncSession, user_id: int, user_data: UserCreate) -> PydanticUser | None:
+    user = await session.get(SQLAUser, user_id)
+    if user:
+        user.username = user_data.username
+        user.hashed_password = hash_password(user_data.password)
+        user.email = user_data.email
+        user.role = user_data.role
+        user.additional_info = user_data.additional_info
+        await session.commit()
+        await session.refresh(user)
+        return PydanticUser.model_validate(user)
+    return None
 
 
 async def get_user(session: AsyncSession, user_info: UserLogin) -> SQLAUser | None:
